7757d891c305a94407d1d3bf83f8392d6f407bb5
Sam_Ash11414 <72979934+SamAsh11414@users.noreply.github.com>
2021-03-25 18:12:22 -0400

    Moved All Files from :D/

---
+++ Keys.py
@@ -0,0 +1,151 @@
+def FirstKey(First_Key):
+    if(First_Key == "C"):
+        First_Key = 'Sounds/C4.mp3'
+    elif(First_Key == "D"):
+        First_Key = 'Sounds/D4.mp3'
+    elif(First_Key == "E"):
+        First_Key = 'Sounds/E4.mp3'
+    elif(First_Key == "F"):
+        First_Key = 'Sounds/F4.mp3'
+    elif(First_Key == "G"):
+        First_Key = 'Sounds/G4.mp3'
+    elif(First_Key == "A"):
+        First_Key = 'Sounds/A4.mp3'
+    elif(First_Key == "B"):
+        First_Key = 'Sounds/B4.mp3'
+    Key1 = []
+    Key1.append({"First_Key": First_Key})
+    return Key1
+
+def SecKey(Sec_Key):
+    if(Sec_Key == "C"):
+        Sec_Key = 'Sounds/C4.mp3'
+    elif(Sec_Key == "D"):
+        Sec_Key = 'Sounds/D4.mp3'
+    elif(Sec_Key == "E"):
+        Sec_Key = 'Sounds/E4.mp3'
+    elif(Sec_Key == "F"):
+        Sec_Key = 'Sounds/F4.mp3'
+    elif(Sec_Key == "G"):
+        Sec_Key = 'Sounds/G4.mp3'
+    elif(Sec_Key == "A"):
+        Sec_Key = 'Sounds/A4.mp3'
+    elif(Sec_Key == "B"):
+        Sec_Key = 'Sounds/B4.mp3'
+    Key2 = []
+    Key2.append({"Sec_Key": Sec_Key})
+    return Key2
+
+def ThreeKey(Three_Key):
+    if(Three_Key == "C"):
+        Three_Key = 'Sounds/C4.mp3'
+    elif(Three_Key == "D"):
+        Three_Key = 'Sounds/D4.mp3'
+    elif(Three_Key == "E"):
+        Three_Key = 'Sounds/E4.mp3'
+    elif(Three_Key == "F"):
+        Three_Key = 'Sounds/F4.mp3'
+    elif(Three_Key == "G"):
+        Three_Key = 'Sounds/G4.mp3'
+    elif(Three_Key == "A"):
+        Three_Key = 'Sounds/A4.mp3'
+    elif(Three_Key == "B"):
+        Three_Key = 'Sounds/B4.mp3'
+    Key3 = []
+    Key3.append({"Three_Key": Three_Key})
+    return Key3
+
+def FourKey(Four_Key):
+    if(Four_Key == "C"):
+        Four_Key = 'Sounds/C4.mp3'
+    elif(Four_Key == "D"):
+        Four_Key = 'Sounds/D4.mp3'
+    elif(Four_Key == "E"):
+        Four_Key = 'Sounds/E4.mp3'
+    elif(Four_Key == "F"):
+        Four_Key = 'Sounds/F4.mp3'
+    elif(Four_Key == "G"):
+        Four_Key = 'Sounds/G4.mp3'
+    elif(Four_Key == "A"):
+        Four_Key = 'Sounds/A4.mp3'
+    elif(Four_Key == "B"):
+        Four_Key = 'Sounds/B4.mp3'
+    Key1 = []
+    Key1.append({"Four_Key": Four_Key})
+    return Key1
+
+def FiveKey(Five_Key):
+    if(Five_Key == "C"):
+        Five_Key = 'Sounds/C4.mp3'
+    elif(Five_Key == "D"):
+        Five_Key = 'Sounds/D4.mp3'
+    elif(Five_Key == "E"):
+        Five_Key = 'Sounds/E4.mp3'
+    elif(Five_Key == "F"):
+        Five_Key = 'Sounds/F4.mp3'
+    elif(Five_Key == "G"):
+        Five_Key = 'Sounds/G4.mp3'
+    elif(Five_Key == "A"):
+        Five_Key = 'Sounds/A4.mp3'
+    elif(Five_Key == "B"):
+        Five_Key = 'Sounds/B4.mp3'
+    Key1 = []
+    Key1.append({"Five_Key": Five_Key})
+    return Key1
+
+def SixKey(Six_Key):
+    if(Six_Key == "C"):
+        Six_Key = 'Sounds/C4.mp3'
+    elif(Six_Key == "D"):
+        Six_Key = 'Sounds/D4.mp3'
+    elif(Six_Key == "E"):
+        Six_Key = 'Sounds/E4.mp3'
+    elif(Six_Key == "F"):
+        Six_Key = 'Sounds/F4.mp3'
+    elif(Six_Key == "G"):
+        Six_Key = 'Sounds/G4.mp3'
+    elif(Six_Key == "A"):
+        Six_Key = 'Sounds/A4.mp3'
+    elif(Six_Key == "B"):
+        Six_Key = 'Sounds/B4.mp3'
+    Key1 = []
+    Key1.append({"Six_Key": Six_Key})
+    return Key1
+
+def SevenKey(Seven_Key):
+    if(Seven_Key == "C"):
+        Seven_Key = 'Sounds/C4.mp3'
+    elif(Seven_Key == "D"):
+        Seven_Key = 'Sounds/D4.mp3'
+    elif(Seven_Key == "E"):
+        Seven_Key = 'Sounds/E4.mp3'
+    elif(Seven_Key == "F"):
+        Seven_Key = 'Sounds/F4.mp3'
+    elif(Seven_Key == "G"):
+        Seven_Key = 'Sounds/G4.mp3'
+    elif(Seven_Key == "A"):
+        Seven_Key = 'Sounds/A4.mp3'
+    elif(Seven_Key == "B"):
+        Seven_Key = 'Sounds/B4.mp3'
+    Key1 = []
+    Key1.append({"Seven_Key": Seven_Key})
+    return Key1
+
+def EightKey(Eight_Key):
+    if(Eight_Key == "C"):
+        Eight_Key = 'Sounds/C4.mp3'
+    elif(Eight_Key == "D"):
+        Eight_Key = 'Sounds/D4.mp3'
+    elif(Eight_Key == "E"):
+        Eight_Key = 'Sounds/E4.mp3'
+    elif(Eight_Key == "F"):
+        Eight_Key = 'Sounds/F4.mp3'
+    elif(Eight_Key == "G"):
+        Eight_Key = 'Sounds/G4.mp3'
+    elif(Eight_Key == "A"):
+        Eight_Key = 'Sounds/A4.mp3'
+    elif(Eight_Key == "B"):
+        Eight_Key = 'Sounds/B4.mp3'
+    Key1 = []
+    Key1.append({"Eight_Key": Eight_Key})
+    return Key1
